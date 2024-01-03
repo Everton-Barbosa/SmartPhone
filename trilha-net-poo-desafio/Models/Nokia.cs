af35eee91b0fb817e7517f7594f54767f24f24ad
Everton Barbosa <115188109+Everton-Barbosa@users.noreply.github.com>
2024-01-02 22:51:40 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Models/Nokia.cs
@@ -0,0 +1,17 @@
+namespace DesafioPOO.Models
+{
+    // Herdar da classe "Smartphone"
+    public class nokia : Smartphone
+    {
+        public nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Nokia ");
+        }
+    }
+}
---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+using System;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("SmartPhone ");
+Smartphone Nokia = new nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111111", memoria: 56);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Telegram");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone");
+Smartphone iphone = new Iphone(numero: "9585", modelo: "Modelo 4", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
